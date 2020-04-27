e8fb56cdf119f083bb9dd490d92d1462c2c8ecee
impactlearner <time.darden@gmail.com>
2020-04-27 20:37:59 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,4 @@
-#$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-#$phrase = "It's-a me, Mario!"
-#$link = "It's Dangerous To Go Alone! Take This."
+
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
